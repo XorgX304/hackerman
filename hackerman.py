c85bbd60d529d87ebc1441760141cf1487718db7
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2019-10-15 01:35:18 +0200

    Update hackerman.py

--- hackerman.py
+++ hackerman.py
@@ -1,3 +1,6 @@
+# Coded By : Khaled Nassar @knassar702
+# Facebook & GITHUB : knassar702
+# GPL-2.0
 from web.run import xss,sqli,ssti,headers
 from rat.pyshell import restart,make_payload,make_connection,restart,slow_print
 from network.scan import whoup,portscanner
